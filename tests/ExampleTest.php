0ee3fa140273affb804532c036f5a6563d1aa067
DamienCahill <53192581+DamienCahill@users.noreply.github.com>
2022-02-12 21:16:35 +0000

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,6 +6,6 @@ use Example\Example;
 class HelloTest extends TestCase {
 
      public function testSayHello() {
-        $this->assertSame( Example::SayHello("world"), "Hello, worlds");
+        $this->assertSame( Example::SayHello("world"), "Hello, world");
     }
 }
